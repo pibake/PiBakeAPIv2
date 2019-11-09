7c3795ade305b2cd8189837e5e9362e4f745fc41
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-11-09 18:16:50 -0500

    filled out a post request

--- request.go
+++ request.go
@@ -1,8 +1,19 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
+	"log"
 	"net/http"
 )
+
+type DatabaseSession struct {
+	session *sql.DB
+}
+
+type JSONResponse struct {
+	Response string `json:"response"`
+}
 
 func GetTemps(w http.ResponseWriter, r *http.Request) {
 	// get all code goes here
@@ -12,8 +23,50 @@ func GetTempOne(w http.ResponseWriter, r *http.Request) {
 	// get one code goes here
 }
 
+// CreateTempOne function that receieves a HTTP POST request
 func CreateTempOne(w http.ResponseWriter, r *http.Request) {
-	// create one code goes here
+	var entry Temp
+	var dbQuery DatabaseSession
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&entry)
+
+	if err != nil {
+		log.Println("Couldn't decode the JSONs!")
+		json.NewEncoder(w).Encode(JSONResponse{"400"})
+	}
+
+	query1, err := dbQuery.session.Query("SELECT * FROM `Pi` WHERE `uuid` = %v ", entry.UUID)
+
+	if err != nil {
+		log.Println("Database goofed: query1")
+		json.NewEncoder(w).Encode(JSONResponse{"400"})
+	}
+
+	if err = query1.Scan(); err != nil {
+		log.Println("New unit sending: inserting UUID for unit.")
+		query2, err := dbQuery.session.Query("INSERT INTO `Pi` (uuid) VALUES %v", entry.UUID)
+
+		defer query2.Close()
+
+		if err != nil {
+			log.Println("Database goofed: query2")
+			json.NewEncoder(w).Encode(JSONResponse{"400"})
+		}
+	}
+
+	query3, err := dbQuery.session.Query("INSERT INTO tempdata (uuid, date, temp_fahrenheit, temp_celsius) VALUES (%v, %v, %v, %v, %v)", entry.UUID,
+		entry.Date,
+		entry.Time,
+		entry.TempF,
+		entry.TempC)
+
+	defer query3.Close()
+
+	if err != nil {
+		log.Println("Database goofed: query3")
+		json.NewEncoder(w).Encode(JSONResponse{"400"})
+	}
 }
 
 func DeleteTempOne(w http.ResponseWriter, r *http.Request) {
