9b0576cd63a85bf48ea4e356bafd77ff539c3054
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-11-16 04:58:04 +0000

    add other stuff

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -8,17 +9,20 @@ import (
 )
 
 func main() {
-	db := NewSQLDB("wyatt", "wyattisawesome", "localhost", "3306", "PiData_Test")
+	// placeholder creds, don't expect them to get you anywhere
+	db := NewSQLDB("wymillerlinux", "123456789", "localhost", "3306", "PiBakeAPIv2")
 	session = db.OpenSQL()
 	defer session.Close()
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api", root).Methods("GET")
-	router.HandleFunc("/api/temps", GetTemps).Methods("GET")
-	router.HandleFunc("/api/temps/{id}", GetTempOne).Methods("GET")
+	router.HandleFunc("/api/temps", GetTemps).Methods("GET") // placeholder
+	router.HandleFunc("/api/temps/{id}", GetTempOne).Methods("GET") // placeholder
 	router.HandleFunc("/api/temps/create", CreateTempOne).Methods("POST")
-	router.HandleFunc("/api/temps/{id}", DeleteTempOne).Methods("DELETE")
+	router.HandleFunc("/api/temps/{id}", DeleteTempOne).Methods("DELETE") // placeholder
+
+	fmt.Println("Starting PiBakeAPI version 2...")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 
