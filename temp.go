595e4def2d59979d161a9a877c857efc3524b756
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-11-22 18:07:13 +0000

    changed punctuation temp model

--- temp.go
+++ temp.go
@@ -2,9 +2,9 @@ package main
 
 // Temp struct, all the JSON fields, all neat and tidy
 type Temp struct {
-	UUID  string `json:"uuid"`
-	Time  string `json:"time"`
-	Date  string `json:"date"`
-	TempF int    `json:"temp_fahrenheit"`
-	TempC int    `json:"temp_celsius"`
+	UUID  string `json:'uuid'`
+	Time  string `json:'time'`
+	Date  string `json:'date'`
+	TempF float64 `json:'temp_fahrenheit'`
+	TempC float64 `json:'temp_celsius'`
 }
