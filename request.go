4a9ef9cc4e174bc7f18c7cb4ab9a456e964de26f
Wyatt J. Miller <wjmiller2016@gmail.com>
2019-11-24 20:59:49 +0000

    fixed go-vet recommendations

--- request.go
+++ request.go
@@ -8,6 +8,7 @@ import (
 
 var session *sql.DB
 
+// JSONResponse struct: Response back to the client in integer form
 type JSONResponse struct {
 	Response int `json:"response"`
 }
@@ -16,14 +17,17 @@ func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "PiBakeAPI v2, written and developed by Wyatt J. Miller, copyright 2019")
 }
 
+// GetTemps function: it's only a placeholder currently
 func GetTemps(w http.ResponseWriter, r *http.Request) {
 	// get all code goes here
 }
 
+// GetTempOne function: it's only a placeholder currently
 func GetTempOne(w http.ResponseWriter, r *http.Request) {
 	// get one code goes here
 }
 
+// DeleteTempOne function: it's only a placeholder currently
 func DeleteTempOne(w http.ResponseWriter, r *http.Request) {
 	// delete one code goes here
 }
